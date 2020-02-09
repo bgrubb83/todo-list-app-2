08db1afe853b89c5151e5244fc422edb7a9c5055
Barry Grubb <bgrubb83@gmail.com>
2020-02-09 16:52:58 +0000

    Implement localStorage setting and getting of todos.

--- src/components/Container.js
+++ src/components/Container.js
@@ -11,6 +11,16 @@ class Container extends React.Component {
         };
     }
 
+    persistToLocalStorage = (key, value) => {
+        console.log('persisting', key, value);
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    readFromLocalStorage = (key) => {
+        console.log('reading', key);
+        return localStorage.getItem(key);
+    }
+
     addTodo = (event, todoText) => {
         event.preventDefault(); 
         const newTodo = { text: todoText, timeStamp: Date.now().toString() }
@@ -23,6 +33,19 @@ class Container extends React.Component {
             return todo.timeStamp && todo.timeStamp !== timeStamp;
         })
         this.setState({ todos: newTodos });
+    }
+
+    componentDidMount = () => {
+        const persistedTodos = this.readFromLocalStorage('todos');
+        if (persistedTodos) {
+            this.setState({ todos: JSON.parse(persistedTodos) });
+        }
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        if (this.state.todos !== prevState.todos) {
+            this.persistToLocalStorage('todos', this.state.todos);
+        }
     }
 
     render() {
